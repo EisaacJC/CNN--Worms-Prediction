affdc7dd3a4cdb4f3625866448a8cb13e0a90dac
Isaac Caballero <eisaacjc8@gmail.com>
2022-06-04 22:21:17 -0500

    crea carpetas de separación

---
+++ folder_creation.sh
@@ -0,0 +1,15 @@
+#Esto es un script para no andar creando las carpetas cada #$%" vez que hagan falta.
+mkdir worms
+cd worms
+mkdir train
+mkdir val
+cd train 
+mkdir alive
+mkdir dead
+cd ..
+cd val
+mkdir alive
+mkdir dead
+exit
+exit
+
